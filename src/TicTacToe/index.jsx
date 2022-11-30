013f3d195831fa47769529e4af13e69a7abe3dac
Joao Carlos <joao.campos22@fatec.sp.gov.br>
2022-11-30 00:53:57 -0300

    Game TicTacToe

--- src/TicTacToe/index.jsx
+++ src/TicTacToe/index.jsx
@@ -1,270 +1,267 @@
 import { useState, React } from 'react'
 import '../App.css'
-import {Train, Play} from './train';
-import { generateRandomWeights } from '../Neuron';
-
-const Tab_sets = [
-    {
-        inputs:[0,0,0,0,0,0,0,0,0],
-        output: 0
-    },
-    {
-        inputs:[
-                1,1,1,
-                0,0,0,
-                0,0,0
-            ], 
-                output:-1
-            },
-    {
-        inputs:[
-                1,-1,1,
-                -1,-1,1,
-                1,1,-1
-            ], 
-                output:0
-            },
-    {
-        inputs:[
-                1,0,0,
-                1,0,0,
-                1,0,0
-            ], 
-                output:-1
-            },
-    {
-        inputs:[
-                1,0,0,
-                0,1,0,
-                0,0,1
-            ], 
-                output:-1
-            },
-    {
-        inputs:[
-                1,-1,1,
-                -1,1,1,
-                -1,1,-1
-            ], 
-                output:0
-            },
-    {
-        inputs:[
-                -1,-1,-1,
-                0,0,0,
-                0,0,0
-            ], 
-                output:1
-            },
-    {
-        inputs:[
-                -1,0,0,
-                -1,0,0,
-                -1,0,0
-            ], 
-                output:1
-            },
-    {
-        inputs:[
-                -1,0,0,
-                0,-1,0,
-                0,0,-1
-            ], 
-                output:1
-            }
-    ]
+import decision_Tree from './decision_Tree';
+let status = "Jogar";
 function Index(){
-    
-    //#region 
-    const [span1,setSpan1] = useState([0, ""]);
-    const [span2,setSpan2] = useState([1, ""]);
-    const [span3,setSpan3] = useState([2, ""]);
-    const [span4,setSpan4] = useState([3, ""]);
-    const [span5,setSpan5] = useState([4, ""]);
-    const [span6,setSpan6] = useState([5, ""]);
-    const [span7,setSpan7] = useState([6, ""]);
-    const [span8,setSpan8] = useState([7, ""]);
-    const [span9,setSpan9] = useState([8, ""]);
-    const [play,setPlay] = useState('X');
-    const [weight,setWeights] = useState(generateRandomWeights);
-    const [tabuleiro,setTabuleiro] = useState([0,0,0,0,0,0,0,0,0]);
-    //#endregion
-   
-    const resultDraw = ()=>{
-        if(span1[1] != "" && span2[1] != "" && span3[1] != "" && span4[1] != "" && span5[1] != "" && span6[1] != "" && span7[1] != "" && span8[1] != "" && span9[1]!="") return true;
+  //Criar uma arvore de decisões para o jogo da velha
+  //inicio
+  const [tabuleiro, setTabuleiro] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+  const [jogador, setJogador] = useState("x"); 
+  //função para jogar
+  function jogar(i, j){
+    if (tabuleiro[i][j] === "") {
+      let novoTabuleiro = [...tabuleiro];
+      novoTabuleiro[i][j] = jogador;
+      setTabuleiro(novoTabuleiro);
+      if (jogador === "x") {
+        setJogador("o");
+      } else {
+        setJogador("x");
+      }
     }
-    const resultX = ()=>{
-        if(span1[1] == "X" && span2[1] == "X" && span3[1] == "X") return true;
-        if(span1[1] == "X" && span5[1] == "X" && span9[1] == "X") return true;
-        if(span1[1] == "X" && span4[1] == "X" && span7[1] == "X") return true;
-        if(span2[1] == "X" && span5[1] == "X" && span8[1] == "X") return true;
-        if(span3[1] == "X" && span6[1] == "X" && span9[1] == "X") return true;
-        if(span3[1] == "X" && span5[1] == "X" && span7[1] == "X") return true;
-        if(span4[1] == "X" && span5[1] == "X" && span6[1] == "X") return true;
-        if(span7[1] == "X" && span8[1] == "X" && span9[1] == "X") return true;
-    }
-    const resultO = ()=>{
-        if(span1[1] == "O" && span2[1] == "O" && span3[1] == "O") return true;
-        if(span1[1] == "O" && span5[1] == "O" && span9[1] == "O") return true;
-        if(span1[1] == "O" && span4[1] == "O" && span7[1] == "O") return true;
-        if(span2[1] == "O" && span5[1] == "O" && span8[1] == "O") return true;
-        if(span3[1] == "O" && span6[1] == "O" && span9[1] == "O") return true;
-        if(span3[1] == "O" && span5[1] == "O" && span7[1] == "O") return true;
-        if(span4[1] == "O" && span5[1] == "O" && span6[1] == "O") return true;
-        if(span7[1] == "O" && span8[1] == "O" && span9[1] == "O") return true;
-    }
-    const marcar = (props)=>{
-        if(props[1] == "" ){
-            setPlay((play == "X"?"O":"X"));
-            if(props[0] === span1[0]) {
-                setSpan1([0,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span2[0]) {
-                setSpan2([1,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span3[0]) {
-                setSpan3([2,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span4[0]) {
-                setSpan4([3,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span5[0]) {
-                setSpan5([4,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span6[0]) {
-                setSpan6([5,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span7[0]) {
-                setSpan7([6,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span8[0]) {
-                setSpan8([7,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
-            if(props[0] == span9[0]) {
-                setSpan9([8,play]);
-                tabuleiro[props[0]] = (play == "X"?1:-1);
-                jogar(tabuleiro[props[0]]);
-            }
+    //verificar a jogada da IA
+    //decision_Tree(tabuleiro)
+  }
+  //Função para IA jogar
+  function jogarIA(){
+    let IA = decision_Tree(tabuleiro);
+    //Irá retornar a ROW e COL
+    let row = IA.row;
+    let col = IA.col;
+    //jogar na IA
+    if (tabuleiro[row][col] === "") {
+        let novoTabuleiro = [...tabuleiro];
+        novoTabuleiro[row][col] = jogador;
+        setTabuleiro(novoTabuleiro);
+        if (jogador === "x") {
+          setJogador("o");
+        } else {
+          setJogador("x");
         }
+      }
+      
+}
+  //função para verificar o vencedor
+  function VerificarVencedor(){
+    //verificar linhas
+    for(let i = 0; i < 3; i++){
+      if(tabuleiro[i][0] === jogador && tabuleiro[i][1] === jogador && tabuleiro[i][2] === jogador){
+        status = "Win "+jogador;
+        return true;
+      }
     }
-    const Reset = ()=>{
-        setSpan1([0,""]);
-        setSpan2([1,""]);
-        setSpan3([2,""]);
-        setSpan4([3,""]);
-        setSpan5([4,""]);
-        setSpan6([5,""]);
-        setSpan7([6,""]);
-        setSpan8([7,""]);
-        setSpan9([8,""]);
-        setPlay("X");
-        let max = Tab_sets.length;
-        let winner = 0;
-        if(resultX) winner = -1;
-        if(resultO) winner = 1;
-        Tab_sets[max] = {
-            inputs: tabuleiro,
-            output: winner
-        };
-        setTabuleiro([0,0,0,0,0,0,0,0,0]);
-        
+    //verificar colunas
+    for(let i = 0; i < 3; i++){
+      if(tabuleiro[0][i] === jogador && tabuleiro[1][i] === jogador && tabuleiro[2][i] === jogador){
+        status = "Win "+jogador;
+        return true;
+      }
     }
-    const exibir = (props)=>{
+    //verificar diagonais
+    if(tabuleiro[0][0] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][2] === jogador){
+      status = "Win "+jogador;
+      return true;
+    }
+    if(tabuleiro[0][2] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][0] === jogador){
+        status = "Win "+jogador;
+        return true;
+    }
+}
+
+  //Função para resetar o jogo
+  function resetar(){
+    setTabuleiro([
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ]);
+    setJogador("x");
+    status = "Jogar";
+  }
+  function Init(){
+    if(VerificarVencedor()){
         return(
             <div>
-                <h1>{props}</h1>
-                <div id='tictactoe'>
-                    <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"1/1/2/2"}} id="span1">{span1[1]}</span>
-                    <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"1/2/2/3"}} id="span2">{span2[1]}</span>
-                    <span style={{borderBottom: "1px solid white", gridArea:"1/3/2/4"}} id="span3">{span3[1]}</span>
-                    <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"2/1/3/2"}} id="span4">{span4[1]}</span>
-                    <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"2/2/3/3"}} id="span5">{span5[1]}</span>
-                    <span style={{borderBottom: "1px solid white", gridArea:"2/3/3/4"}} id="span6">{span6[1]}</span>
-                    <span style={{borderRight:"1px solid white", gridArea:"3/1/4/2"}} id="span7">{span7[1]}</span>
-                    <span style={{borderRight:"1px solid white", gridArea:"3/2/4/3"}} id="span8">{span8[1]}</span>
-                    <span style={{gridArea:"3/3/4/4"}} id="span9">{span9[1]}</span>
-                </div>
-                <button onClick={()=>{
-                    Reset();
-                    }}>
-                Treinar
-            </button>
-            </div>)
-    }
-    const jogar = (props) => {
-        
-        let vez = (props==-1?1:-1);
-        let newTabuleiro = Play(tabuleiro,vez,Tab_sets);
-        setTabuleiro(newTabuleiro);
-        console.log(newTabuleiro);
-        //#region 
-        if(span1[1] == "" && newTabuleiro[0] == -1) setSpan1([0,"O"])
-        if(span2[1] == "" && newTabuleiro[1] == -1) setSpan2([1,"O"])
-        if(span3[1] == "" && newTabuleiro[2] == -1) setSpan3([2,"O"])
-        if(span4[1] == "" && newTabuleiro[3] == -1) setSpan4([3,"O"])
-        if(span5[1] == "" && newTabuleiro[4] == -1) setSpan5([4,"O"])
-        if(span6[1] == "" && newTabuleiro[5] == -1) setSpan6([5,"O"])
-        if(span7[1] == "" && newTabuleiro[6] == -1) setSpan7([6,"O"])
-        if(span8[1] == "" && newTabuleiro[7] == -1) setSpan8([7,"O"])
-        if(span9[1] == "" && newTabuleiro[8] == -1) setSpan9([8,"O"])
-        if(span1[1] == "" && newTabuleiro[0] == 1) setSpan1([0,"X"])
-        if(span2[1] == "" && newTabuleiro[1] == 1) setSpan2([1,"X"])
-        if(span3[1] == "" && newTabuleiro[2] == 1) setSpan3([2,"X"])
-        if(span4[1] == "" && newTabuleiro[3] == 1) setSpan4([3,"X"])
-        if(span5[1] == "" && newTabuleiro[4] == 1) setSpan5([4,"X"])
-        if(span6[1] == "" && newTabuleiro[5] == 1) setSpan6([5,"X"])
-        if(span7[1] == "" && newTabuleiro[6] == 1) setSpan7([6,"X"])
-        if(span8[1] == "" && newTabuleiro[7] == 1) setSpan8([7,"X"])
-        if(span9[1] == "" && newTabuleiro[8] == 1) setSpan9([8,"X"])
-        //#endregion
-        setPlay((play == "X"?"O":"X"));
-    }
+                <h1>TicTacToe</h1>
+                <h2>{status}</h2>
+                <div id="tictactoe">
+            <span
+                style={{
+                borderBottom: "1px solid white",
+                borderRight: "1px solid white",
+                gridArea: "1/1/2/2",
+                }}
+                id="span1"
+            >
+                {tabuleiro[0][0]}
+            </span>
+            <span
+                style={{
+                borderBottom: "1px solid white",
+                borderRight: "1px solid white",
+                gridArea: "1/2/2/3",
+                }}
+                id="span2"
+            >
+                {tabuleiro[0][1]}
+            </span>
+            <span
+                style={{ borderBottom: "1px solid white", gridArea: "1/3/2/4" }}
+                id="span3"
     
-    if(resultX()){
-    return exibir("Win X");
-    }if(resultO()){
-    return exibir("Win O");
-    }if(resultDraw()){
-    return(exibir("Empate"));
-    }
-    
-    return(
-        <div>
-            <h1>TicTacToe</h1>
-            <div id='tictactoe'>
-                <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"1/1/2/2"}} id="span1">{span1[1]}</span>
-                <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"1/2/2/3"}} id="span2">{span2[1]}</span>
-                <span style={{borderBottom: "1px solid white", gridArea:"1/3/2/4"}} id="span3">{span3[1]}</span>
-                <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"2/1/3/2"}} id="span4">{span4[1]}</span>
-                <span style={{borderBottom: "1px solid white", borderRight:"1px solid white", gridArea:"2/2/3/3"}} id="span5">{span5[1]}</span>
-                <span style={{borderBottom: "1px solid white", gridArea:"2/3/3/4"}} id="span6">{span6[1]}</span>
-                <span style={{borderRight:"1px solid white", gridArea:"3/1/4/2"}} id="span7">{span7[1]}</span>
-                <span style={{borderRight:"1px solid white", gridArea:"3/2/4/3"}} id="span8">{span8[1]}</span>
-                <span style={{gridArea:"3/3/4/4"}} id="span9">{span9[1]}</span>
+            >
+                {tabuleiro[0][2]}
+            </span>
+            <span
+                style={{
+                borderBottom: "1px solid white",
+                borderRight: "1px solid white",
+                gridArea: "2/1/3/2",
+                }}
+                id="span4"
+            >
+                {tabuleiro[1][0]}
+            </span>
+            <span
+                style={{
+                borderBottom: "1px solid white",
+                borderRight: "1px solid white",
+                gridArea: "2/2/3/3",
+                }}
+                id="span5"
+            >
+                {tabuleiro[1][1]}
+            </span>
+            <span
+                style={{ borderBottom: "1px solid white", gridArea: "2/3/3/4" }}
+                id="span6"
+            >
+                {tabuleiro[1][2]}
+            </span>
+            <span
+                style={{ borderRight: "1px solid white", gridArea: "3/1/4/2" }}
+                id="span7"
+            >
+                {tabuleiro[2][0]}
+            </span>
+            <span
+                style={{ borderRight: "1px solid white", gridArea: "3/2/4/3" }}
+                id="span8"
+            >
+                {tabuleiro[2][1]}
+            </span>
+            <span
+                style={{ gridArea: "3/3/4/4" }}
+                id="span9"
+            >
+                {tabuleiro[2][2]}
+            </span>
             </div>
-            <button onClick={()=>{
-                jogar((play == "O"?1:-1))
-            }}>
-                IA
+            <button id="resetar" onClick={resetar}>
+                Resetar
             </button>
-        </div>
+      </div>
+        )
+    }
+    return(
+        <div >
+    <h1>TicTacToe</h1>
+    <h2>{status}</h2>
+    <div id="tictactoe">
+      <span
+        style={{
+          borderBottom: "1px solid white",
+          borderRight: "1px solid white",
+          gridArea: "1/1/2/2",
+        }}
+        id="span1"
+        onClick={() => jogar(0, 0)}
+      >
+        {tabuleiro[0][0]}
+      </span>
+      <span
+        style={{
+          borderBottom: "1px solid white",
+          borderRight: "1px solid white",
+          gridArea: "1/2/2/3",
+        }}
+        id="span2"
+        onClick={() => jogar(0, 1)}
+      >
+        {tabuleiro[0][1]}
+      </span>
+      <span
+        style={{ borderBottom: "1px solid white", gridArea: "1/3/2/4" }}
+        id="span3"
+        onClick={() => jogar(0, 2)}
+      >
+        {tabuleiro[0][2]}
+      </span>
+      <span
+        style={{
+          borderBottom: "1px solid white",
+          borderRight: "1px solid white",
+          gridArea: "2/1/3/2",
+        }}
+        id="span4"
+        onClick={() => jogar(1, 0)}
+      >
+        {tabuleiro[1][0]}
+      </span>
+      <span
+        style={{
+          borderBottom: "1px solid white",
+          borderRight: "1px solid white",
+          gridArea: "2/2/3/3",
+        }}
+        id="span5"
+        onClick={() => jogar(1, 1)}
+      >
+        {tabuleiro[1][1]}
+      </span>
+      <span
+        style={{ borderBottom: "1px solid white", gridArea: "2/3/3/4" }}
+        id="span6"
+        onClick={() => jogar(1, 2)}
+      >
+        {tabuleiro[1][2]}
+      </span>
+      <span
+        style={{ borderRight: "1px solid white", gridArea: "3/1/4/2" }}
+        id="span7"
+        onClick={() => jogar(2, 0)}
+      >
+        {tabuleiro[2][0]}
+      </span>
+      <span
+        style={{ borderRight: "1px solid white", gridArea: "3/2/4/3" }}
+        id="span8"
+        onClick={() => jogar(2, 1)}
+      >
+        {tabuleiro[2][1]}
+      </span>
+      <span
+        style={{ gridArea: "3/3/4/4" }}
+        id="span9"
+        onClick={() => jogar(2, 2)}
+      >
+        {tabuleiro[2][2]}
+      </span>
+    </div>
+    <button id="resetar" onClick={resetar}>
+        Resetar
+    </button>
+    <button id="Ia" onClick={()=>jogarIA()}>Jogar IA</button>
+  </div>
     )
+  }
+return (
+  <Init></Init>
+); 
 }
 
 export default Index
