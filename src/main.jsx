67e103c494c56f580dbc0065ac576e0812e73a0e
Joao Carlos <joao.campos22@fatec.sp.gov.br>
2022-12-01 14:18:32 -0300

    1.0.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App'
 import Index from './TicTacToe/index'
+import { render } from 'react-dom'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Index />
-  </React.StrictMode>
+document.getElementById('root').render(
+    <React.StrictMode>
+      <Index />
+    </React.StrictMode>
 )
