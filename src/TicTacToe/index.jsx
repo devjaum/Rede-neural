896e961cae137c777f667c8161d8a481cac1ad01
Joao Carlos <joao.campos22@fatec.sp.gov.br>
2022-12-02 15:00:56 -0300

    1.0.2

--- src/TicTacToe/index.jsx
+++ src/TicTacToe/index.jsx
@@ -17,6 +17,8 @@ function Index(){
       let novoTabuleiro = [...tabuleiro];
       novoTabuleiro[i][j] = 'x';
       setTabuleiro(novoTabuleiro);
+      if(!VerificarEmpate()) jogarIA();
+      else status="Empate";
     }
     //verificar a jogada da IA
     //decision_Tree(tabuleiro)
@@ -39,29 +41,64 @@ function Index(){
   function VerificarVencedor(){
     //verificar linhas
     for(let i = 0; i < 3; i++){
-      if(tabuleiro[i][0] === jogador && tabuleiro[i][1] === jogador && tabuleiro[i][2] === jogador){
-        status = "Win "+jogador;
+      if(tabuleiro[i][0] === "x" && tabuleiro[i][1] === "x" && tabuleiro[i][2] === "x"){
+        status = "Win "+"X";
         return true;
       }
     }
     //verificar colunas
     for(let i = 0; i < 3; i++){
-      if(tabuleiro[0][i] === jogador && tabuleiro[1][i] === jogador && tabuleiro[2][i] === jogador){
-        status = "Win "+jogador;
+      if(tabuleiro[0][i] === "x" && tabuleiro[1][i] === "x" && tabuleiro[2][i] === "x"){
+        status = "Win "+"X";
         return true;
       }
     }
     //verificar diagonais
-    if(tabuleiro[0][0] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][2] === jogador){
-      status = "Win "+jogador;
+    if(tabuleiro[0][0] === "x" && tabuleiro[1][1] === "x" && tabuleiro[2][2] === "x"){
+      status = "Win "+"X";
       return true;
     }
-    if(tabuleiro[0][2] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][0] === jogador){
-        status = "Win "+jogador;
+    if(tabuleiro[0][2] === "x" && tabuleiro[1][1] === "x" && tabuleiro[2][0] === "x"){
+        status = "Win "+"X";
+        return true;
+    }
+    for(let i = 0; i < 3; i++){
+      if(tabuleiro[i][0] === "o" && tabuleiro[i][1] === "o" && tabuleiro[i][2] === "o"){
+        status = "Win "+"O";
+        return true;
+      }
+    }
+    //verificar colunas
+    for(let i = 0; i < 3; i++){
+      if(tabuleiro[0][i] === "o" && tabuleiro[1][i] === "o" && tabuleiro[2][i] === "o"){
+        status = "Win "+"O";
+        return true;
+      }
+    }
+    //verificar diagonais
+    if(tabuleiro[0][0] === "o" && tabuleiro[1][1] === "o" && tabuleiro[2][2] === "o"){
+      status = "Win "+"O";
+      return true;
+    }
+    if(tabuleiro[0][2] === "o" && tabuleiro[1][1] === "o" && tabuleiro[2][0] === "o"){
+        status = "Win "+"O";
         return true;
     }
 }
-
+  //Função para verificar se empatou
+  function VerificarEmpate(){
+    let cont = 0;
+    for (let i = 0; i < 3; i++) {
+      for(let j = 0; j < 3; j++){
+        if (tabuleiro[i][j] === "") {
+          cont++;
+        }
+      }
+    }
+    console.log(cont);
+    if(cont != 0) return false;
+    return true;
+  }
   //Função para resetar o jogo
   function resetar(){
     setTabuleiro([
@@ -69,7 +106,6 @@ function Index(){
       ["", "", ""],
       ["", "", ""],
     ]);
-    setJogador("x");
     status = "Jogar";
   }
   function Init(){
@@ -169,7 +205,7 @@ function Index(){
           gridArea: "1/1/2/2",
         }}
         id="span1"
-        onClick={() => {jogar(0, 0);jogarIA()}}
+        onClick={() => {jogar(0, 0)}}
       >
         {tabuleiro[0][0]}
       </span>
@@ -180,14 +216,14 @@ function Index(){
           gridArea: "1/2/2/3",
         }}
         id="span2"
-        onClick={() => {jogar(0, 1);jogarIA()}}
+        onClick={() => {jogar(0, 1)}}
       >
         {tabuleiro[0][1]}
       </span>
       <span
         style={{ borderBottom: "1px solid white", gridArea: "1/3/2/4" }}
         id="span3"
-        onClick={() => {jogar(0, 2);jogarIA()}}
+        onClick={() => {jogar(0, 2)}}
       >
         {tabuleiro[0][2]}
       </span>
@@ -198,7 +234,7 @@ function Index(){
           gridArea: "2/1/3/2",
         }}
         id="span4"
-        onClick={() => {jogar(1, 0);jogarIA()}}
+        onClick={() => {jogar(1, 0)}}
       >
         {tabuleiro[1][0]}
       </span>
@@ -209,35 +245,35 @@ function Index(){
           gridArea: "2/2/3/3",
         }}
         id="span5"
-        onClick={() => {jogar(1, 1);jogarIA()}}
+        onClick={() => {jogar(1, 1)}}
       >
         {tabuleiro[1][1]}
       </span>
       <span
         style={{ borderBottom: "1px solid white", gridArea: "2/3/3/4" }}
         id="span6"
-        onClick={() => {jogar(1, 2);jogarIA()}}
+        onClick={() => {jogar(1, 2)}}
       >
         {tabuleiro[1][2]}
       </span>
       <span
         style={{ borderRight: "1px solid white", gridArea: "3/1/4/2" }}
         id="span7"
-        onClick={() => {jogar(2, 0);jogarIA()}}
+        onClick={() => {jogar(2, 0)}}
       >
         {tabuleiro[2][0]}
       </span>
       <span
         style={{ borderRight: "1px solid white", gridArea: "3/2/4/3" }}
         id="span8"
-        onClick={() => {jogar(2, 1);jogarIA()}}
+        onClick={() => {jogar(2, 1)}}
       >
         {tabuleiro[2][1]}
       </span>
       <span
         style={{ gridArea: "3/3/4/4" }}
         id="span9"
-        onClick={() => {jogar(2, 2);jogarIA()}}
+        onClick={() => {jogar(2, 2)}}
       >
         {tabuleiro[2][2]}
       </span>
