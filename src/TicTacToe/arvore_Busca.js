ff6145c2475e1b0f8cb482a9fb6eca6e8033c48a
Joao Carlos <joao.campos22@fatec.sp.gov.br>
2022-12-04 18:48:53 -0300

    1.1

--- src/TicTacToe/arvore_Busca.js
+++ src/TicTacToe/arvore_Busca.js
@@ -1,112 +1,122 @@
-let winner;
-let eficiente_winner = [];
-let stop = false;
-let i = 0;
-let p = 0;
-let soma = 0;
-let min = [];
-let aux = [
-    {p:Number, soma:Number}
-];
-export const init = (tabuleiro, vez) => {
-    let newTabuleiro = [...tabuleiro];
-    arvore(tabuleiro,vez);
-}
-function arvore (tabuleiro,vez){
-   
-    while(!stop){
-        if(tabuleiro[i] == 0) return transicao(tabuleiro,vez);
-        i = parseInt(Math.random()*9);
-        //x,0,x|0,x,0|x
-        if(tabuleiro.indexOf(0) == -1) return transicao(tabuleiro,vez);
+class Movimento{
+    constructor(){
+        let linha, coluna;
     }
 }
-
-function transicao (tabuleiro, vez) {
-    soma++;
-    var tabuleiro_auxiliar = [...tabuleiro];
-    tabuleiro_auxiliar[i] = vez;
-    //console.log(winner);
-    if(!vencer_tabuleiro(tabuleiro_auxiliar)) return arvore(tabuleiro_auxiliar,(vez == 1?-1:1));
-    let x = parseInt(Math.random()*9)
-    
-    i = ( x== i?i+1:x);
-    eficiente(winner);
-    //console.log(tabuleiro_vazio);
-    return arvore([0,0,0,0,0,0,0,0,0],vez);
-}
-
-function vencer_tabuleiro(tabuleiro) {
-
-    if((tabuleiro[0] == tabuleiro[1]) && (tabuleiro[2] == tabuleiro[1]) && tabuleiro[0] !=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    if((tabuleiro[3] == tabuleiro[4]) && (tabuleiro[3] == tabuleiro[5]) && tabuleiro[5]!=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    if((tabuleiro[6] == tabuleiro[7]) && (tabuleiro[6] == tabuleiro[8]) && tabuleiro[8]!=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    if((tabuleiro[0] == tabuleiro[3]) && (tabuleiro[0] == tabuleiro[6]) && tabuleiro[6]!=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    if((tabuleiro[1] == tabuleiro[4]) && (tabuleiro[1] == tabuleiro[7]) && tabuleiro[7]!=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    if((tabuleiro[2] == tabuleiro[5]) && (tabuleiro[2] == tabuleiro[8]) && tabuleiro[8]!=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    if((tabuleiro[0] == tabuleiro[4]) && (tabuleiro[0] == tabuleiro[8]) && tabuleiro[8]!=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    if((tabuleiro[2] == tabuleiro[4]) && (tabuleiro[2] == tabuleiro[6]) && tabuleiro[6]!=0){
-        winner = [...tabuleiro];
-        return true;
-    }
-    return false;
-}
-
-export const eficiente = (winner_) => {
-    aux.unshift({p:p},{soma:soma})
-   // aux.fill(soma=soma)
-    //aux[p].soma = soma;
-    p++;
-    //soma++;
-    eficiente_winner.push(winner_);
-    soma = 0;
-    if(p == 500) {return stop=true};
-}
-function imprimir(x){
-    let y = [...x]
-    for (let i = 0; i < y.length; i++) {
-        if(y[i] == 1) y[i] = "X";
-        if(y[i] == -1) y[i] = "O";
-        if(y[i] == 0) y[i] = " ";
-    }
-    console.log("\n",y[0],y[1],y[2],"\n",y[3],y[4],y[5],"\n",y[6],y[7],y[8],"\n",);
-}
-let vez = 1; 
-let auxiliar;
-init([0,0,0,0,0,0,0,0,0],vez);
-for (let i = 0; i < aux.length; i++) {
-    for (let j = 0; j < aux.length; j++) {
-        if(aux[j].soma!=undefined){
-            if(aux[i].soma<aux[j].soma) {
-                aux[j].soma = aux[i].soma;
-                auxiliar = aux[i-1].p;
-            }
+//Iniciando os jogadores
+let CPU = "o", jogador = "x";
+//Verificando se ainda tem jogadas a serem feitas
+const movimentos = (tabuleiro) =>{
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if(tabuleiro[i][j] == "") return true;
             
         }
     }
+    return false;
 }
-console.log(auxiliar, aux[1].soma);
-
-for(let i = 0; i < winner.length;i++){if(winner[i] == 1)winner[i] = "X";else if(winner[i] == -1) winner[i] = "O"; else winner[i] = i}
-imprimir(eficiente_winner[auxiliar])
+//Função para avaliar se venceu e retornar em valores de pontuação
+const avaliacao = (tab) => {
+    //Verificando horizontalmente se venceu
+    for(let linha = 0; linha < 3; linha++){
+        if(tab[linha][0] == tab[linha][1] && tab[linha][1] == tab[linha][2]){
+            if(tab[linha][0] == CPU) return +1;
+            else if(tab[linha][0] == jogador) return -1;
+        }
+    }
+    //Verificando verticalmente se venceu
+    for(let coluna = 0; coluna < 3; coluna++){
+        if(tab[0][coluna] == tab[1][coluna] && tab[1][coluna] == tab[2][coluna]){
+            if(tab[0][coluna] == CPU) return +1;
+            else if(tab[0][coluna] == jogador) return -1;
+        }
+    }
+    //Verificando a diagonal principal se venceu
+    if (tab[0][0] == tab[1][1] && tab[1][1] == tab[2][2])
+    {
+        if (tab[0][0] == CPU) return +1;
+        else if (tab[0][0] == jogador) return -1;
+    }
+    //Verificando a diagonal secundaria se venceu
+    if (tab[0][2] == tab[1][1] &&
+        tab[1][1] == tab[2][0])
+    {
+        if (tab[0][2] == CPU)
+            return +1;
+             
+        else if (tab[0][2] == jogador)
+            return -1;
+    }
+  
+    //Se nenhum vencer, retornar 0.
+    return 0;
+} 
+//Função para retornar todos os caminhos possíveis para o jogo e
+//Retorna o melhor para o tabuleiro
+const arvore = (tabuleiro, profundidade, max) => {
+    let score = avaliacao(tabuleiro);
+    //Retornando o valor maximo ou minimo do score
+    if(score == 1 || score == -1) return score;
+    //Retornando 0 caso seja empate
+    if(movimentos(tabuleiro) == false) return 0;
+    //Pegando o melhor movimento
+    if(max){
+        let melhor = -1000;
+        //Verificando todas as posições e fazendo o movimento se estiver vazio
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 3; j++){
+                if(tabuleiro[i][j] == ""){
+                    tabuleiro[i][j] = CPU;
+                    //Pegar o valor max através da recursividade
+                    melhor = Math.max(melhor, arvore(tabuleiro,profundidade+1,!max));
+                    //Desfazer o movimento
+                    tabuleiro[i][j] = "";
+                }
+            }
+        }
+        return melhor;
+        //Pegando o pior movimento
+    }else{
+        let melhor = +1000;
+        //Verificando todas as posições e fazendo o movimento se estiver vazio
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 3; j++){
+                if(tabuleiro[i][j] === ""){
+                    tabuleiro[i][j] = jogador;
+                    //Pegar o valor min através da recursividade
+                    melhor = Math.min(melhor, arvore(tabuleiro,profundidade+1,!max));
+                    //Desfazer o movimento
+                    tabuleiro[i][j] = "";
+                }
+            }
+        }
+        return melhor;
+    }
+}
+//Buscando o melhor movimento para o CPU 
+export const buscarMovimento = (tabuleiro) =>{
+    if(tabuleiro === undefined) return;
+    //Iniciando
+    let melhorValor = -1;
+    let melhorMovimento = new Movimento();
+    melhorMovimento.linha = -1;
+    melhorMovimento.coluna = -1;
+    //Laço de repetição para achar a melhor jogada
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            if(tabuleiro[i][j] === ""){
+                //Adicionando a jogada
+                tabuleiro[i][j] = CPU;
+                let valorMovimento = arvore(tabuleiro, 0, false);
+                //Verificando se a jogada é a melhor possivel
+                if(valorMovimento > melhorValor){
+                    melhorMovimento.linha = i;
+                    melhorMovimento.coluna = j;
+                    melhorValor = valorMovimento;
+                }
+                tabuleiro[i][j] = "";
+            }
+        }
+    }
+    return melhorMovimento;
+}
